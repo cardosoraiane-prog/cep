b17d251028cbcaae76547787e36b8574121da986
SeuNome <SeuEmail>
2026-04-13 11:19:18 -0300

    pc

--- src/script.js
+++ src/script.js
@@ -6,4 +6,15 @@ if (!cep){
 }
 fetch(`https://viacep.com.br/ws/${cep}/json`)
 .then(response=>response.json())
+.then(data=>{
+    if(data.error){
+        resultado.innerHTML="<ps style=color:red>cep  não encontrado</p>" 
+    } else{
+        resultado.innerHTML=`
+        <p>lougradouro:${data.loigaradouro}</p>
+        <p>bairro:${data.bairro}</p>
+        <p>cidade:${data.localidade}</p>
+        <p>estado:${data.uf}</p>`; 
+    }
+})
 }
